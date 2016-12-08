d1adb614c1154cdb09a84f7cc65bc67fee5d9e00
ssolovev <me@sergeysolovev.com>
2016-12-09 01:49:51 +0800

    Add separate / route that redirects to /people

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import { BrowserRouter, Match, Miss, Link } from 'react-router'
-import PeopleContainer from '../containers/PeopleContainer.js'
-import About from '../components/About.js'
-import NoMatch from '../components/NoMatch.js'
+import { BrowserRouter, Match, Miss, Link, Redirect } from 'react-router'
+import PeopleContainer from '../containers/PeopleContainer'
+import About from '../components/About'
+import NoMatch from '../components/NoMatch'
 
 const App = () => (
   <BrowserRouter>
     <div>
       <ul>
         <li><Link to="/">Home</Link></li>
+        <li><Link to="/people">People</Link></li>
         <li><Link to="/about">About</Link></li>
       </ul>
       <hr />
-      <Match exactly pattern="/" component={PeopleContainer} />
+      <Match exactly pattern="/" render={() => (<Redirect to="/people" />)} />
+      <Match pattern="/people" component={PeopleContainer} />
       <Match pattern="/about" component={About} />
       <Miss component={NoMatch} />
     </div>
