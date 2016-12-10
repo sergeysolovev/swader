8e0c3916066a31e3e6124c3eaddca51a15468c93
ssolovev <me@sergeysolovev.com>
2016-12-10 15:21:54 +0800

    Remove odd functions from routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,5 @@ export const PLANETS = "/planets";
 export const STARSHIPS = "/starships";
 export const ABOUT = "/about";
 
-export const getPersonPath = personId => `people/${personId}`;
-export const getPlanetPath = planetId => `planets/${planetId}`;
 export const getResourcePath = (resourceType, resourceId) =>
   `${resourceType}/${resourceId}`
