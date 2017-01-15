1eece56c334ba5d9055c91fc225adfbe64860959
ssolovev <me@sergeysolovev.com>
2017-01-15 13:42:53 +0800

    Update About page

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About = () => (
   <div className='container'>
-    <h2>About SWADER</h2>
+    <h2>About Swader</h2>
+    <p>
+      Stands for The Star Wars API's Data Explorer,
+      an open source <a href="https://facebook.github.io/react/">React</a> app for exloring the data provided by <a href="http://swapi.co">The Star Wars API</a>.
+    </p>
   </div>
 )
 export default About
