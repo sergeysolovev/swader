d5718d4c5bd4b76956b88e3e0d0678a0b2c026e4
ssolovev <me@sergeysolovev.com>
2017-01-15 13:43:59 +0800

    Update Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,11 @@ import { Link } from 'react-router'
 
 const Home = () => (
   <div className='hero'>
-    <h2>SWADER</h2>
+    <h2>Swader</h2>
     <p>
       Stands for The Star Wars API's Data Explorer,
       an open source <a href="https://facebook.github.io/react/">React</a> app for exloring the data provided by <a href="http://swapi.co">The Star Wars API</a>.
     </p>
-
     <Link to='/films' className='btn btn-alt'>Start exploring</Link>
   </div>
 )
