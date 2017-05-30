bdf992444b33393f5f9c6a1d2a1a32b8cd776902
ssolovev <me@sergeysolovev.com>
2017-05-31 04:16:37 +0800

    Update Main component

---
+++ Tasks.md
@@ -0,0 +1,20 @@
+# swader todo
+
+## tasks
+- responsive markup for mobile devices
+- use redux (need experience)
+- add tests;
+- DONE: update resource list page (search/navigate)
+
+## other tasks
+- update packages versions (new router, new react)
+- make film resources clickable
+- add dependencies (router is a must, redux and indent would be good);
+- DONE: deploy
+
+## fixes
+- DONE: remove warnings produced by api.js
+
+## maybe
+- make ident as a separate js lib
+- fetching animation (it's a cool mix beetween text-only and react)
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,33 +6,20 @@ import * as Routes from '../routes'
 import Film from '../containers/Film'
 import Films from '../containers/Films'
 import ResourceList from '../containers/ResourceList'
-import PlanetItem from '../components/PlanetItem'
-import PeopleItem from '../components/PeopleItem'
-import StarshipItem from '../components/StarshipItem'
-import Person from '../containers/Person'
 import About from '../components/About'
 import NoMatch from '../components/NoMatch'
 
 const Main = () => (
   <main>
     <Match exactly pattern='/' component={About} />
-
     <Match exactly pattern={Routes.FILMS} component={Films} />
     <Match pattern={`${Routes.FILMS}/:filmId`} component={Film} />
-
-    <Match pattern={`${Routes.PEOPLE}/:personId`} component={Person} />
     <Match exactly pattern={Routes.PEOPLE} render={(matchProps) =>
-      <ResourceList {...matchProps}
-        resourceType='people'
-        itemComponent={PeopleItem} />} />
+      <ResourceList {...matchProps} resourceType='people' />} />
     <Match exactly pattern={Routes.PLANETS} render={(matchProps) =>
-      <ResourceList {...matchProps}
-        resourceType='planets'
-        itemComponent={PlanetItem} />} />
+      <ResourceList {...matchProps} resourceType='planets' />} />
     <Match exactly pattern={Routes.STARSHIPS} render={(matchProps) =>
-      <ResourceList {...matchProps}
-        resourceType='starships'
-        itemComponent={StarshipItem} />} />
+      <ResourceList {...matchProps} resourceType='starships' />} />
     <Miss component={NoMatch} />
   </main>
 )
