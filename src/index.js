aeb78b599415aa78d131dc395528171b50a7f82a
ssolovev <me@sergeysolovev.com>
2017-06-19 11:20:49 +0800

    Add registerServiceWorker, manifest.json, noscript

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import { render } from 'react-dom'
 import './reset.css'
 import './index.css'
 import App from './components/App'
+import registerServiceWorker from './registerServiceWorker' 
 
 render(<App />, document.querySelector('#root'));
+registerServiceWorker();
